170495d6654a647c9696e4f1d1a1e5cf3bd4aed8
Jack Lawson <ajacksified@gmail.com>
2014-11-17 10:12:07 -0800

    Rebase on latest and re-build. Such diff wow

--- dist/app/dashboards/scripted.js
+++ dist/app/dashboards/scripted.js
@@ -28,16 +28,13 @@ timspan = '1d';
 // Intialize a skeleton with nothing but a rows array and service object
 dashboard = {
   rows : [],
-  services : {}
 };
 
 // Set a title
 dashboard.title = 'Scripted dash';
-dashboard.services.filter = {
-  time: {
-    from: "now-" + (ARGS.from || timspan),
-    to: "now"
-  }
+dashboard.time = {
+  from: "now-" + (ARGS.from || timspan),
+  to: "now"
 };
 
 var rows = 1;
@@ -59,7 +56,7 @@ for (var i = 0; i < rows; i++) {
     panels: [
       {
         title: 'Events',
-        type: 'graphite',
+        type: 'graph',
         span: 12,
         fill: 1,
         linewidth: 2,
@@ -71,6 +68,17 @@ for (var i = 0; i < rows; i++) {
             'target': "randomWalk('random walk2')"
           }
         ],
+        seriesOverrides: [
+          {
+            alias: '/random/',
+            yaxis: 2,
+            fill: 0,
+            linewidth: 5
+          }
+        ],
+        tooltip: {
+          shared: true
+        }
       }
     ]
   });
